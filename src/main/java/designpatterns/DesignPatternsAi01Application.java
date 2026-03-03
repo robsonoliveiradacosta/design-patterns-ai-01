63cbeb3fde7e3cc6c70c496afcd4759488ed5e6b
Robson Oliveira Da Costa <robson.costa@gmail.com>
2026-03-02 22:39:10 -0400

    Initialize Spring Boot project with Maven support. Include Maven Wrapper, `.gitignore` for common build artifacts, and initial application setup.

---
+++ src/main/java/designpatterns/DesignPatternsAi01Application.java
@@ -0,0 +1,13 @@
+package designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsAi01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsAi01Application.class, args);
+    }
+
+}
